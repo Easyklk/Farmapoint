6ca83952465cfb163004e12edd1a93fb963f3bf8
Easyklk <isaacromanillos@gmail.com>
2022-04-04 13:45:01 +0200

    4-4-2022 13:44

--- Farmapoint/MainWindow.xaml.cs
+++ Farmapoint/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Data.OleDb;
+using System.Windows;
 
 namespace Farmapoint
 {
@@ -14,49 +15,49 @@ namespace Farmapoint
 
         private void Button_Login(object sender, RoutedEventArgs e)
         {
-            this.Hide();
-            Window1 ventanaLogeado = new Window1();
-            ventanaLogeado.Show();
+            //this.Hide();
+            //Window1 ventanaLogeado = new Window1();
+            //ventanaLogeado.Show();
 
-            //OleDbConnection conexion = ConexionDb.AbrirConexion();
-            //if (string.IsNullOrEmpty(Txt_numFarmacia.Text) && string.IsNullOrEmpty(Txt_User.Text))
-            //{
-            //    Txt_numFarmacia.BorderBrush = System.Windows.Media.Brushes.Red;
-            //    Txt_User.BorderBrush = System.Windows.Media.Brushes.Red;
-            //    login_error.Text = "¡Rellene todos los campos!";
-            //}
-            //else if (string.IsNullOrEmpty(Txt_numFarmacia.Text))
-            //{
-            //    Txt_User.BorderBrush = System.Windows.Media.Brushes.Black;
-            //    Txt_numFarmacia.BorderBrush = System.Windows.Media.Brushes.Red;
-            //    login_error.Text = "¡Rellene el campo Nº Farmacia!";
-            //}
-            //else if (string.IsNullOrEmpty(Txt_User.Text))
-            //{
-            //    Txt_numFarmacia.BorderBrush = System.Windows.Media.Brushes.Black;
-            //    Txt_User.BorderBrush = System.Windows.Media.Brushes.Red;
-            //    login_error.Text = "¡Rellene el campo Usuario!";
-            //}
-            //else
-            //{
-            //    OleDbCommand adapter = new OleDbCommand("SELECT * FROM CSOAPHeadUsr WHERE Usuario = '" + Txt_User.Text + "'" + "AND Farmacia= '" + Txt_numFarmacia.Text + "'", conexion);
-            //    OleDbDataReader dr = adapter.ExecuteReader();
-            //    DataSet d = new DataSet();
-            //    if (dr.Read())
-            //    {
-            //        conexion.Close();
-            //        this.Hide();
+            OleDbConnection conexion = ConexionDb.AbrirConexion();
+            if (string.IsNullOrEmpty(Txt_numFarmacia.Text) && string.IsNullOrEmpty(Txt_User.Text))
+            {
+                Txt_numFarmacia.BorderBrush = System.Windows.Media.Brushes.Red;
+                Txt_User.BorderBrush = System.Windows.Media.Brushes.Red;
+                login_error.Text = "¡Rellene todos los campos!";
+            }
+            else if (string.IsNullOrEmpty(Txt_numFarmacia.Text))
+            {
+                Txt_User.BorderBrush = System.Windows.Media.Brushes.Black;
+                Txt_numFarmacia.BorderBrush = System.Windows.Media.Brushes.Red;
+                login_error.Text = "¡Rellene el campo Nº Farmacia!";
+            }
+            else if (string.IsNullOrEmpty(Txt_User.Text))
+            {
+                Txt_numFarmacia.BorderBrush = System.Windows.Media.Brushes.Black;
+                Txt_User.BorderBrush = System.Windows.Media.Brushes.Red;
+                login_error.Text = "¡Rellene el campo Usuario!";
+            }
+            else
+            {
+                OleDbCommand adapter = new OleDbCommand("SELECT * FROM CSOAPHeadUsr WHERE Usuario = '" + Txt_User.Text + "'" + "AND Farmacia= " + int.Parse(Txt_numFarmacia.Text)+";", conexion);
+                OleDbDataReader dr = adapter.ExecuteReader();
+                if (dr.Read())
+                {
+                    Window1 ventanaLogeado = new Window1();
+                    ventanaLogeado.Show();
 
-            //        Window1 ventanaLogeado = new Window1();
-            //        ventanaLogeado.Show();
-            //    }
-            //    else
-            //    {
-            //        Txt_numFarmacia.BorderBrush = System.Windows.Media.Brushes.Red;
-            //        Txt_User.BorderBrush = System.Windows.Media.Brushes.Red;
-            //        login_error.Text = "¡Nº Farmacia o Usuario incorrectos!";
-            //    }
-            //}
+                    conexion.Close();
+                    this.Hide();
+
+                }
+                else
+                {
+                    Txt_numFarmacia.BorderBrush = System.Windows.Media.Brushes.Red;
+                    Txt_User.BorderBrush = System.Windows.Media.Brushes.Red;
+                    login_error.Text = "¡Nº Farmacia o Usuario incorrectos!";
+                }
+            }
 
         }
     }
