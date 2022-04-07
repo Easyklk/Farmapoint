69dcfa737513b639cc35f131809dc04c1af192a5
Easyklk <isaacromanillos@gmail.com>
2022-04-07 11:40:35 +0200

    11:40

--- Farmapoint/VentanaDetallesMedicamentosDispensables.xaml.cs
+++ Farmapoint/VentanaDetallesMedicamentosDispensables.xaml.cs
@@ -14,28 +14,25 @@ namespace Farmapoint
         private OleDbCommand command = new OleDbCommand();
         private DataSet d = new DataSet();
         private string codigoSns;
+        private CRecetaDispensable recetaDispensable;
 
         public VentanaDetallesMedicamentosDispensables(CRecetaDispensable recetaDispensable, string codigoSns)
         {
             InitializeComponent();
             this.codigoSns = codigoSns;
+            this.recetaDispensable = recetaDispensable;
         }
 
         private void rellenarDatos(OleDbConnection conexion, OleDbDataAdapter adapter, OleDbCommand command, DataSet d)
         {
             try
             {
-                d.Clear();
-                string qry = "SELECT CRecetaDispensable.* " +
-                    "FROM(CRecetaDispensable LEFT JOIN CBusquedaReferenciasOUT " +
-                    "ON CRecetaDispensable.Identificador_Receta = CBusquedaReferenciasOUT.RecetasDispensable) " +
-                    "INNER JOIN(CPaciente INNER JOIN CBusquedaReferenciasIN ON CPaciente.ID_Paciente = CBusquedaReferenciasIN.ID_Paciente) " +
-                    "ON CRecetaDispensable.Identificador_Receta = CBusquedaReferenciasIN.Receta_Dispensable " +
-                    "WHERE CPaciente.Codigo_SNS = '" + codigoSns + "'";
+                d.Clear();  
+                string qry = "SELECT CRecetaDispensada.* FROM((CRecetaDispensable INNER JOIN CRecetaDispensada ON CRecetaDispensable.Identificador_Receta = CRecetaDispensada.Identificador_Receta) INNER JOIN CRecetaCDA ON CRecetaDispensable.Identificador_Receta = CRecetaCDA.Identificador_Receta) INNER JOIN(CPaciente INNER JOIN CBusquedaReferenciasIN ON CPaciente.ID_Paciente = CBusquedaReferenciasIN.ID_Paciente) ON CRecetaDispensable.Identificador_Receta = CBusquedaReferenciasIN.Receta_Dispensable WHERE(((CRecetaDispensada.Identificador_Receta) = '1'));";
                 command.CommandText = qry;
                 command.Connection = conexion;
                 adapter.SelectCommand = command;
-                adapter.Fill(d, "CRecetaDispensable" + "CBusquedaReferenciasOUT" + "CPaciente");
+                adapter.Fill(d, "CRecetaCDA");
                 conexion.Close();
             }
             catch (Exception ex)
@@ -53,7 +50,9 @@ namespace Farmapoint
 
         private void DataRow_Loaded(object sender, RoutedEventArgs e)
         {
-
+            grdDatos.ItemsSource = null;
+            rellenarDatos(conexion, adapter, command, d);
+            grdDatos.ItemsSource = d.Tables["CRecetaCDA" ].DefaultView;
         }
 
         private void label_nombre_Loaded(object sender, RoutedEventArgs e)
