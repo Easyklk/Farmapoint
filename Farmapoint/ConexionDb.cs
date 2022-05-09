f9fd3c8982d086cae02c235afe6af03f0dc00105
Easyklk <isaacromanillos@gmail.com>
2022-05-09 11:03:51 +0200

    @Easyklk

--- Farmapoint/ConexionDb.cs
+++ Farmapoint/ConexionDb.cs
@@ -6,7 +6,7 @@ namespace Farmapoint
     {
         public static OleDbConnection AbrirConexion()
         {
-            OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=C:/Users/Practica/Documents/Farmapointdb.accdb");
+            OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=|DataDirectory|/Farmapoint.accdb");
             conexion.Open();
             return conexion;
         }
